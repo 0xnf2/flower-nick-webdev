c9e0e762be696b7c4fd83b8dca47f859e249c40f
nf <flower.n@husky.neu.edu>
2016-10-20 20:49:21 -0700

    Bind Controllers to their Routes and Views

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -7,42 +7,66 @@
         //User Views
             .when("/login", { //login
                 templateUrl: "views/user/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
             })
             .when("/register", { //Register
-                templateUrl: "views/user/register.view.client.html"
+                templateUrl: "views/user/register.view.client.html",
+                controller: "RegisterController",
+                controllerAs: "model"
             })
             .when("/user/:uid", { //profile
-                templateUrl: "views/user/profile.view.client.html"
+                templateUrl: "views/user/profile.view.client.html",
+                controller: "ProfileController",
+                controllerAs: "model"
             })
             //Website views
             .when("/user/:uid/website", { //website-list
                 templateUrl: "views/website/website-list.view.client.html",
+                controller: "WebsiteListController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/new", { //website-new
-                templateUrl: "views/website/website-new.view.client.html"
+                templateUrl: "views/website/website-new.view.client.html",
+                controller: "NewWebsiteController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/:wid", { //website-edit
-                templateUrl: "views/website/website-edit.view.client.html"
+                templateUrl: "views/website/website-edit.view.client.html",
+                controller: "EditWebsiteController",
+                controllerAs: "model"
             })
              //Page Views
             .when("/user/:uid/website/:wid/page", { //page-list
-                templateUrl: "views/page/page-list.view.client.html"
+                templateUrl: "views/page/page-list.view.client.html",
+                controller: "PageListController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/new", { //page-new
-                templateUrl: "views/page/page-new.view.client.html"
+                templateUrl: "views/page/page-new.view.client.html",
+                controller: "NewPageController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid", { //page-edit
-                templateUrl: "views/page/page-edit.view.client.html"
+                templateUrl: "views/page/page-edit.view.client.html",
+                controller: "EditPageController",
+                controllerAs: "model"
             })
             //Widget Views
             .when("/user/:uid/website/:wid/page/:pid/widget", { //Widget-List
-                templateUrl: "views/widget/widget-list.view.client"
+                templateUrl: "views/widget/widget-list.view.client.html",
+                controller: "WidgetListController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid/widget/new", { //Widget-new
-                templateUrl: "views/widget/widget-choose.view.client"
+                templateUrl: "views/widget/widget-choose.view.client.html",
+                controller: "NewWidgetController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", { //Widget-edit
-                templateUrl: "views/widget/widget-edit.view.client"
+                templateUrl: "views/widget/widget-edit.view.client.html",
+                controller: "EditWidgetController",
+                controllerAs: "model"
             })
             .otherwise({
                 redirectTo: "/login"
