c75cafcb47b5e49d7f36f96a54e353516fd712ed
nf <flower.n@husky.neu.edu>
2016-10-20 19:57:34 -0700

    index loads service scripts

---
+++ public/assignment/services/user.service.client.js
@@ -0,0 +1,17 @@
+// (function() {
+//     angular
+//   .module("WebAppMaker")
+//   .factory("UserService", UserService);
+//     function UserService() {
+//       var users = [ ... ];
+//       var api = {
+//     "createUser"   : "createUser",
+//     "findUserById" : "findUserById",
+//     ...
+//   };
+//   return api;
+//   function createUser(user) { ... }
+//   function findUserById(id) { ... }
+//   ...
+//     }
+// })();
