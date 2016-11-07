e5092b1d40a410a88e4cb5f61fd64ebd9c088646
nf <flower.n@husky.neu.edu>
2016-11-06 21:51:55 -0800

    added sortable(?)

--- public/assignment/directives/jga-directives.js
+++ public/assignment/directives/jga-directives.js
@@ -3,7 +3,21 @@
         .module("WebAppMaker", [])
         .directive("jga-sortable", jgasortable);
     
-    function jgasortable() {
-        console.log("Hello from Sortable");
+    function jgasortable($http) {
+        $(function () {
+            var url = "/page/:pageId/widget?initial=index1&final=index2";
+            var sortable = $("jga-sortable");
+            let initial = -1;
+            sortable.sortable({
+                start:function(event, ui){
+                    initial = $(ui.item).index();
+                },
+                update:function(event,ui){
+                    let final = $(ui.item).index();
+                    var url = "/page/"+pageId+"/widget?initial="+initial+"&final="+final;
+                    $http.get(url);
+                }
+            });
+        });
     }
 })();
