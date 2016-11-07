a42614b26e74fa438fe078dfe63e2d7ab8c165f1
nf <flower.n@husky.neu.edu>
2016-11-06 21:19:56 -0800

    new website controller uses promises

--- public/assignment/views/website/newwebsite.controller.client.js
+++ public/assignment/views/website/newwebsite.controller.client.js
@@ -6,10 +6,16 @@
         var vm = this;
         vm.userId = $routeParams.uid;
         vm.websiteId = $routeParams.wid;
-
         vm.createWebsite = createWebsite;
         function createWebsite(name, description){
-            WebsiteService.createWebsite(vm.userId, {"name":name, "description":description, "developerId": vm.userId});
+            let promise = WebsiteService.createWebsite(vm.userId, {"name":name, "description":description, "developerId": vm.userId});
+            promise
+                .success(function(website){
+                    console.log("success");
+            })
+            .error(function(err){
+                     console.log("error: " + err);
+            });
         }
      }
 })();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
