66ff7989ff043ec6be0f52d3e55ae83c149789ee
nf <flower.n@husky.neu.edu>
2016-11-06 21:06:08 -0800

    user.service.website uses

--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -2,16 +2,7 @@
     angular
   .module("WebAppMaker")
   .factory("WebsiteService", WebsiteService);
-    function WebsiteService() {
-    //   var websites = [
-    //             { "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem" },
-    //             { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem" },
-    //             { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
-    //             { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
-    //             { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
-    //             { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
-    //         ];
-
+    function WebsiteService($http) {
       var api = {
             "createWebsite"   : createWebsite,
             "findWebsitesByUser" : findWebsitesByUser,
@@ -21,40 +12,29 @@
         };
   return api;
 
-//   function createWebsite(userId, website) { 
-//       website.developerId = userId;
-//       websites.push(website);
-//    }
+  function createWebsite(userId, website) { 
+      var url = "/api/user/"+userId+"/website";
+      return $http.post(url, website);
+   }
 
-//   function findWebsitesByUser(userId)  {
-//       return websites.filter(function(val){return val.developerId === userId;});
-//   }
+  function findWebsitesByUser(userId)  {
+      var url = "/api/user/"+userId+"/website";
+      return $http.get(url);
+  }
 
-//   function findWebsiteById(websiteId){
-//       var result = websites.filter(function(val){return val._id === websiteId;});
-//       if(result.length !== 0){
-//           return result[0];
-//       }else{
-//           return null;
-//       }
-//   }
+  function findWebsiteById(websiteId){
+      var url = "/api/website/"+websiteId;
+      return $http.get(url);
+  }
 
-//   function updateWebsite(websiteId, website){
-//       for(var i = 0; i < websites.length; i++){
-//           if(websites[i]._id === websiteId){
-//               websites[i] = website;
-//               return;
-//           }
-//         }
-//     }
+  function updateWebsite(websiteId, website){
+      var url = "/api/website/"+websiteId;
+      return $http.put(url, website);
+  }
 
-//   function deleteWebsite(websiteId){
-//       for(var i = 0; i < websites.length; i++){
-//           if(websites[i]._id === websiteId){
-//               websites.splice(i, 1);
-//               return;
-//           }
-//         }
-//     }
+  function deleteWebsite(websiteId){
+      var url = "/api/website/"+websiteId;
+      return $http.delete(url);
+    }
 }
 })();
