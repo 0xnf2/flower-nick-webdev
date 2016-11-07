095db2da4053e1240fb2a119283aab269c4b44b7
nf <flower.n@husky.neu.edu>
2016-11-06 21:02:16 -0800

    user.service.client uses

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -2,15 +2,7 @@
     angular
   .module("WebAppMaker")
   .factory("UserService", UserService);
-    function UserService() {
-        
-        //Moved
-    //   var users = [
-    //         {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-    //         {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-    //         {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-    //         {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-    //     ];
+    function UserService($http) {
 
       var api = {
             "createUser"   : createUser,
@@ -22,60 +14,34 @@
         };
   return api;
 
-//Moved
-//   function createUser(user) { 
-//       users.push(user);
-//    }
+  function createUser(user) { 
+      var url = "/api/user/";
+      return $http.post(url, user);
+   }
 
-//moved
-//   function findUserById(id) { 
-//       var result = users.filter(function(val){return val._id === id;});
-//       if(result.length !== 0){
-//           return result[0];
-//       }else{
-//           return null;
-//       }
-//   }
+  function findUserById(id) { 
+      var url = "/api/user?username="+id;
+      return $http.get(url);
+  }
 
-//moved
-//   function findUserByUsername(username){
-//       var result = users.filter(function(val){return val.username === username;});
-//       if(result.length !== 0){
-//           return result[0];
-//       }else{
-//           return null;
-//       }
-//   }
+  function findUserByUsername(username){
+      var url = "/api/user?username="+username;
+      return $http.get(url);
+  }
 
+  function findUserByCredentials(username, password){
+      var url = "/api/user?username="+username+"&password="+password;
+      return $http.get(url);
+  }
 
-//Moved
-//   function findUserByCredentials(username, password){
-//       var result = users.filter(function(val){return val.username === username && val.password === password;});
-//       if(result.length !== 0){
-//           return result[0];
-//       }else{
-//           return null;
-//       }
-//   }
+  function updateUser(userId, user){
+      var url = "/api/user/"+userId;
+      return $http.put(url, user);
+    }
 
-//moved
-//   function updateUser(userId, user){
-//       for(var i = 0; i < users.length; i++){
-//           if(users[i]._id === userId){
-//               users[i] = user;
-//               return;
-//           }
-//         }
-//     }
-
-//moved
-//   function deleteUser(userId){
-//       for(var i = 0; i < users.length; i++){
-//           if(users[i]._id === userId){
-//               users.splice(i, 1);
-//               return;
-//           }
-//         }
-//     }
+  function deleteUser(userId){
+      var url = "/api/user/"+userId;
+      return $http.delete(url);
+    }
 }
 })();
