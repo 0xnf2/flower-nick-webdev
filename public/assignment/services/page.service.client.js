3df58d156c24c84b057688fed506d3e415bc337f
nf <flower.n@husky.neu.edu>
2016-11-06 21:08:58 -0800

    user.service.page uses

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -2,12 +2,7 @@
     angular
   .module("WebAppMaker")
   .factory("PageService", PageService);
-    function PageService() {
-    //   var pages = [
-    //         { "_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem" },
-    //         { "_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem" },
-    //         { "_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem" }
-    //     ];
+    function PageService($http) {
 
       var api = {
             "createPage"   : createPage,
@@ -18,40 +13,29 @@
         };
   return api;
 
-//   function createPage(websiteId, page){ 
-//       page.websiteId = websiteId;
-//       pages.push(page);
-//    }
+  function createPage(websiteId, page){ 
+      var url = "/api/website/"+websiteId+"/page";
+      return $http.post(url, page);
+   }
 
-//   function findPagesByWebsiteId(websiteId)  { 
-//       return pages.filter(function(val){return val.websiteId === websiteId;});
-//   }
+  function findPagesByWebsiteId(websiteId)  { 
+      var url = "/api/website/"+websiteId+"/page";
+      return $http.get(url);
+  }
 
-//   function findPageById(pageId){
-//       var result = pages.filter(function(val){return val._id === pageId;});
-//       if(result.length !== 0){
-//           return result[0];
-//       }else{
-//           return null;
-//       }
-//   }
+  function findPageById(pageId){
+      var url = "/api/page/"+pageId;
+      return $http.get(url);
+  }
 
-//   function updatePage(pageId, page){
-//       for(var i = 0; i < pages.length; i++){
-//           if(pages[i]._id === pageId){
-//               pages[i] = page;
-//               return;
-//           }
-//         }
-//     }
+  function updatePage(pageId, page){
+      var url = "/api/page/"+pageId;
+      return $http.put(url, page);
+    }
 
-//   function deletePage(pageId){
-//       for(var i = 0; i < pages.length; i++){
-//           if(pages[i]._id === pageId){
-//               pages.splice(i, 1);
-//               return;
-//           }
-//         }
-//     }
+  function deletePage(pageId){
+      var url = "/api/page/"+pageId;
+      return $http.delete(url);
+    }
 }
 })();
